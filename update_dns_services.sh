23b393a7d51e8f7c0349ba7cfb48052dd3272e00
cjonesrun <cjonesrun@gmail.com>
2020-08-19 16:36:06 -0400

    update

--- update_dns_services.sh
+++ update_dns_services.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-#FreeDNS updater script
+#FreeDNS and StrongDNS update script
 
 set -x
 . /home/pi/freedns.apikey
 . /home/pi/strongdns_update_url.key
 
-echo "[`date`] updating freedns entry at http://sync.afraid.org/u/$KEY/" >> /var/log/freedns_update.log
+LOG1=/var/log/freedns_update.log
+TMPLOG1=/tmp/tmp1.log
+cat $LOG1 > $TMPLOG1
+
+echo "[`date`] updating freedns entry at http://sync.afraid.org/u/$KEY/" >> $TMPLOG1 2>&1 &
 OUTPUT=`curl http://sync.afraid.org/u/$KEY/` 
-echo "[`date`] $OUTPUT" >> /var/log/freedns_update.log
+echo "[`date`] $OUTPUT" >> $TMPLOG1 2>&1 &
+sudo mv $TMPLOG1 $LOG1
+
+LOG2=/var/log/strongdns_update.log
+TMPLOG2=/tmp/tmp2.log
+cat $LOG2 > $TMPLOG2
 
 # update the dns service in the event that my ip changes...
-echo "[`date`] updating ip at $DNS_SERVICE_URL" >> /var/log/strongdns_update.log 2>&1 &
+echo "[`date`] updating ip at $DNS_SERVICE_URL" >> $TMPLOG2 2>&1 &
 OUTPUT=`curl $DNS_SERVICE_URL`
-echo "[`date`] $OUTPUT" >> /var/log/strongdns_update.log 2>&1 &
-
+echo "[`date`] $OUTPUT" >> $TMPLOG2 2>&1 &
+sudo mv $TMPLOG2 $LOG2
